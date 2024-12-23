b1f6d02b59435d1e5d365312948b80cc0dcc175d
Robert Pankowecki <robert.pankowecki@gmail.com>
2024-12-23 20:10:03 +0100

    not working yet

---
+++ res_incremental_reduce/README.md
@@ -0,0 +1,73 @@
+# About this example
+
+This [example](https://github.com/paneq/pg_playground/blob/main/res_incremental/res_pg_incremental.rb) 
+demonstrates how [pg_incremental](https://github.com/CrunchyData/pg_incremental) extension can
+be used for processing domain events' data
+coming from [Rails Event Store](https://railseventstore.org/)
+to build materialized [read models](https://event-driven.io/en/projections_and_read_models_in_event_driven_architecture/).
+
+The script publishes `UserRegistered` events and then pg_incremental processes 
+them every minute to build a `registrations` read model which shows how many users
+were registered in each minute and from which social network.
+
+Artificial sleeps are added to transactions to make them last longer and to demonstrate
+how pg_incremental processes them safely:
+
+> The pipeline execution ensures that the range of sequence values is known to be
+> safe, meaning that there are no more transactions that might produce sequence values
+> that are within the range. This is ensured by waiting for concurrent write transactions
+> before proceeding with the command. The size of the range is effectively the number of inserts
+> since the last time the pipeline was executed up to the moment that the new pipeline
+> execution started.
+
+# Setup
+
+```
+bundle install
+cd .. && docker compose up --build
+```
+
+# Running the example
+
+```
+bundle exec ruby res_pg_incremental.rb
+```
+
+# Example output
+
+```
+creating: 7830
+creating: 871
+creating: 7840
+creating: 7850
+creating: 7860
+creating: 7870
+creating: 7880
+creating: 7890
+creating: 7900
+creating: 7910
+Stopping writers
+creating: 7920
+creating: 881
+Minute: 2024-12-23 13:18:00 UTC, Source: bsky, Total: 3
+Minute: 2024-12-23 13:18:00 UTC, Source: facebook, Total: 41
+Minute: 2024-12-23 13:18:00 UTC, Source: linkedin, Total: 365
+++++
+Total in read model: 409, Total in event store: 884
+---- to stop workers run: kill -HUP 3153
+creating: 52
+Minute: 2024-12-23 13:18:00 UTC, Source: bsky, Total: 3
+Minute: 2024-12-23 13:18:00 UTC, Source: facebook, Total: 41
+Minute: 2024-12-23 13:18:00 UTC, Source: linkedin, Total: 365
+++++
+Total in read model: 409, Total in event store: 885
+---- to stop workers run: kill -HUP 3153
+Minute: 2024-12-23 13:19:00 UTC, Source: bsky, Total: 2
+Minute: 2024-12-23 13:19:00 UTC, Source: facebook, Total: 47
+Minute: 2024-12-23 13:19:00 UTC, Source: linkedin, Total: 427
+Minute: 2024-12-23 13:18:00 UTC, Source: bsky, Total: 3
+Minute: 2024-12-23 13:18:00 UTC, Source: facebook, Total: 41
+Minute: 2024-12-23 13:18:00 UTC, Source: linkedin, Total: 365
+++++
+Total in read model: 885, Total in event store: 885
+```
---
+++ res_incremental_reduce/res_pg_incremental_reduce.rb
@@ -0,0 +1,144 @@
+require 'active_record'
+require 'logger'
+require 'rails_event_store_active_record'
+require 'rails_event_store/all'
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'postgresql',
+  host: 'localhost',
+  database: 'postgres',
+  username: 'postgres',
+  password: 'postgres',
+  port: 5432
+)
+
+# ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+class CreateRESTable < ActiveRecord::Migration[7.0]
+  def up
+    drop_table :event_store_events, if_exists: true
+    drop_table :event_store_events_in_streams, if_exists: true
+
+    create_table "event_store_events", id: :serial, force: :cascade do |t|
+      t.uuid "event_id", null: false
+      t.string "event_type", null: false
+      t.jsonb "metadata"
+      t.jsonb "data", null: false
+      t.datetime "created_at", precision: nil, null: false
+      t.datetime "valid_at", precision: nil
+      t.index ["created_at"], name: "index_event_store_events_on_created_at"
+      t.index ["event_id"], name: "index_event_store_events_on_event_id", unique: true
+      t.index ["event_type"], name: "index_event_store_events_on_event_type"
+      t.index ["valid_at"], name: "index_event_store_events_on_valid_at"
+    end
+
+    create_table "event_store_events_in_streams", id: :serial, force: :cascade do |t|
+      t.string "stream", null: false
+      t.integer "position"
+      t.uuid "event_id", null: false
+      t.datetime "created_at", precision: nil, null: false
+      t.index ["created_at"], name: "index_event_store_events_in_streams_on_created_at"
+      t.index ["event_id"], name: "index_event_store_events_in_streams_on_event_id"
+      t.index ["stream", "event_id"], name: "index_event_store_events_in_streams_on_stream_and_event_id", unique: true
+      t.index ["stream", "position"], name: "index_event_store_events_in_streams_on_stream_and_position", unique: true
+    end
+  end
+end
+
+class CreateUsers < ActiveRecord::Migration[8.0]
+  def up
+    drop_table :users, if_exists: true
+    create_table "users", force: :cascade do |t|
+      t.string "status", null: false
+    end
+  end
+end
+
+class ProcessUserEvents < ActiveRecord::Migration[8.0]
+  def up
+    begin
+      # TODO: Fix me
+      execute("select incremental.drop_pipeline('reduce-user-events');")
+    rescue
+      puts "rescued"
+    end
+    sql = <<-SQL
+      select incremental.create_sequence_pipeline('reduce-user-events', 'event_store_events',
+        $$
+          DECLARE user_id integer;
+          BEGIN
+          FOR e IN SELECT * FROM event_store_events
+          WHERE 
+            id BETWEEN $1 AND $2
+          AND
+            event_type IN ('UserRegistered', 'UserUnregistered')
+          LOOP
+            user_id := (e.payload->>'user_id')::integer;
+            IF e.event_type = 'UserRegistered' THEN
+              INSERT INTO users (id, status) VALUES ((e.payload->>'user_id')::integer, 'registered');
+            ELSIF e.event_type = 'UserUnregistered' THEN
+              UPDATE users SET status = 'regequit' 
+              WHERE id = (e.payload->>'user_id')::integer;
+            ELSE
+              RAISE NOTICE 'Unexpected event type: %', e.event_type;  
+            END IF;
+          END LOOP;
+          END;
+        $$
+      );
+    SQL
+    execute(sql)
+  end
+end
+
+CreateRESTable.new.up
+CreateUsers.new.up
+ProcessUserEvents.new.up
+
+event_store = RailsEventStore::JSONClient.new
+
+class User < ActiveRecord::Base
+  self.table_name = 'users'
+end
+
+class UserRegistered < RailsEventStore::Event
+end
+class UserUnregistered < RailsEventStore::Event
+end
+
+stop_writing = false
+Signal.trap("HUP") do
+  puts "Stopping writers"
+  stop_writing = true
+end
+
+Thread.new do
+  (1..).each do |user_id|
+    break if stop_writing
+    ActiveRecord::Base.transaction do
+      puts "registering: #{user_id}"
+      event_store.publish(UserRegistered.new(data: {
+        user_id: user_id,
+        name: "User #{user_id}",
+      }))
+
+      if user_id % 3 == 1
+        puts "unregistering: #{user_id}"
+        event_store.publish(UserUnregistered.new(data: {
+          user_id: user_id,
+        }))
+      end
+    end
+  end
+end
+
+
+loop do
+  sleep(10)
+  ActiveRecord::Base.transaction do
+    User.order('id DESC').limit(10).each do |user|
+      puts "User: #{user.id}, Status: #{user.status}"
+    end
+    puts "---- to stop producers run: kill -HUP #{Process.pid}"
+  end
+end
